b7eaeb5677492742bc4a28091c4989f2bb8908e9
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-07 13:32:53 -0600

    added XHR to load messages.json

--- chatty.js
+++ chatty.js
@@ -1,1 +1,7 @@
 console.log("Linked!")
+
+// XML Http Request
+var msgRequest = new XMLHttpRequest()
+msgRequest.addEventListener("load", loadMessages)
+msgRequest.open("GET", "messages.json")
+msgRequest.send()
