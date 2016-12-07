2cdd9769699d620f002ad8bca656947da26598ca
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-07 14:25:50 -0600

    added funtion to parse JSON

--- chatty.js
+++ chatty.js
@@ -5,3 +5,9 @@ var msgRequest = new XMLHttpRequest()
 msgRequest.addEventListener("load", loadMessages)
 msgRequest.open("GET", "messages.json")
 msgRequest.send()
+
+// funtion to parse JSON
+function loadMessages (e) {
+   var presetMsgs = JSON.parse(e.target.responseText)
+   console.log(presetMsgs)
+}
