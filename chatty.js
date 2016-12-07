d78627971c5ff05a183f4d3bea6d1057570b9a55
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-07 15:26:41 -0600

    working on function to fill DOM and event listener

--- chatty.js
+++ chatty.js
@@ -5,11 +5,23 @@ var msgRequest = new XMLHttpRequest()
 msgRequest.addEventListener("load", loadMessages)
 msgRequest.open("GET", "messages.json")
 msgRequest.send()
-<<<<<<< HEAD
-=======
 
 // funtion to parse JSON
+var presetMsgs
 function loadMessages (e) {
-   var presetMsgs = JSON.parse(e.target.responseText)
+   presetMsgs = JSON.parse(e.target.responseText)
    console.log(presetMsgs)
 }
+
+//function to populate DOM message content
+var messageContent
+for (var i = 0; i < presetMsgs.stockMessages.length; i++) {
+  messageContent += `
+                    <<div>
+                        <h3 id="msg${i}">${data.stockMessages[i].content}</h3>
+                    </div>
+                    `
+console.log(messageContent)
+}
+
+//Event listener for load event
