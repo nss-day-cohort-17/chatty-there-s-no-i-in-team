82ec86065ca58ca59737753ffac7b1b0d813a171
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-08 15:04:06 -0600

    searching for delete button obj in console

--- chatty.js
+++ chatty.js
@@ -1,4 +1,4 @@
-console.log("Linked!")
+// console.log("Linked!")
 
 // XML Http Request
 var msgRequest = new XMLHttpRequest()
@@ -21,12 +21,12 @@ function messages (presetMsgs) {
       for (var i = 0; i < presetMsgs.stockMessages.length; i++) {
       messageContent += `
                         <div>
-                          <h3 id="msg${i}">${presetMsgs.stockMessages[i].content}</h3>
-                          <button id=clear${i}>Delete</button>
+                          <h3>${presetMsgs.stockMessages[i].content}</h3>
+                            <button>Delete</button>
                         </div>
                         `
     document.getElementById("message_box").innerHTML = messageContent;
-    console.log(messageContent)
+    // console.log(messageContent)
   }
 }
 
@@ -39,20 +39,12 @@ function messages (presetMsgs) {
 // largeText.addEventListener()
 
 
-//Event listener for load event
-//event listener still needs 2nd argument and function
-  //for keyboard event
-
-
-// var existingMessages = document.getElementById("msg1");
-// existingMessages.addEventListener('load', section);
-
 
 //Event listener to display new message when enter key is pressed
 document.getElementById("comment").addEventListener('keypress', function(x) {
    if (x.key === "Enter") {
       var newComment = document.getElementById("comment").value
-      console.log("new Comment", newComment)
+      // console.log("new Comment", newComment)
       document.getElementById("message_box").innerHTML += `
                <div>
                   <h3>${newComment}</h3>
@@ -65,5 +57,16 @@ document.getElementById("comment").addEventListener('keypress', function(x) {
 
 // funtion and event listener for "Clear all messages" button
 
+document.getElementById("clear").addEventListener("click", function() {
+    document.getElementById("message_box").innerHTML = " ";
+})
 
-var clearAll = document.getElementById("clear", )
+//function and listener for individual message delete buttons
+
+// document.getElementById("message_box").addEventListener("click", function() {
+//   if ()
+// }
+
+document.getElementById("message_box").addEventListener("click", function(e) {
+  console.dir(e.target)
+})
